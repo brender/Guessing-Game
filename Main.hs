1da2497512c8d13c77b4a2d408177f0469fe0a88
Brenda McIntosh <brenda.k.mcintosh@gmail.com>
2013-04-03 20:11:19 -0700

    Start a Main file

---
+++ Main.hs
@@ -0,0 +1,12 @@
+-- To run the program:
+--
+-- Using GHCi:
+--		1. Run "ghci main.hs".
+--		2. Run the "main" function.
+--		3. To reload after changes are made, run ":reload".
+--
+-- Using GHC:
+--		1. Run "./build.sh".
+--		2. Run "./guessing". The "guessing" file is the program.
+
+main = putStrLn "Hello World"
